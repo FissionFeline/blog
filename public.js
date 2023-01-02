fc271024fc7066f65e0311b69975fbc953e17f06
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-01-02 23:56:51 +0100

    Update public.js

--- public.js
+++ public.js
@@ -1,1 +1,25 @@
+module.exports = (function() {
+    var route = require('express').Router();
 
+    route.get('/', function(req, res, next) {
+        res.send('');
+    });
+
+    route.get('/:user/:type', function(req, res, next) {
+        if (req.params.type == 0) {
+            next('route');
+        }
+        res.send(`You gave us ${req.params.type}`)
+    });
+
+    route.get('/:user/', function(req, res, next) {
+        if (req.params.type == 0) {
+            next('route');
+        }
+        res.send(`You gave us ${req.params.type}`)
+    });
+    route.use((req, res, next) => {
+        res.send("Give us sth will ya ?");
+    });
+    return route;
+})();
