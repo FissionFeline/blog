589ff004bf97b49ba354f9cb9a0f5ca37a48fdc4
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-01-02 11:46:04 +0100

    Mongodb configuration

--- db_handler.js
+++ db_handler.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 class mongo_connection {
     constructor() {
         const mongo = new MongoClient(process.env.MLINK);
+        this.article = mongo.db("articles")
+        this.users = mongo.db("users")
     }
 }
 
