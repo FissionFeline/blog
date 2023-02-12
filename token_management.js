00e8801f7e44bdc88066821d54ec65b5566473ec
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-02-12 21:48:58 +0100

    Update token_management.js

--- token_management.js
+++ token_management.js
@@ -1,4 +1,20 @@
 const jwt = require("jwt")
 const generate_token = (ID) => {
     return jwt.sign(ID, process.env.SECRET, { expiresIn: '1800s' });
+};
+
+const verify_token = (token) => {
+    jwt.verify(token, process.env.SECRET, (err, ID) => {
+
+        if (err) return [err, null]
+
+        if (ID) return [null, err]
+
+        return ["Unknown error", null]
+    })
+}
+
+module.exports = {
+    verify_token,
+    generate_token
 }
