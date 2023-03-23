3df9e26f8eb4a15b8af71aa3bddabf27d139629a
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-03-23 23:53:15 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -7,33 +7,5 @@ module.exports = (function() {
 
     })
 
-    route.get('/:user/:article', function(req, res, next) {
-        let params = {}
-        try {
-            params.object_id = new ObjectId(req.params.article)
-            params.user = req.params.user
-        } catch (err) {
-            return next()
-        }
-        db.articles.findOne({ _id: params.object_id, author_id: params.user }, {}, function(err, result) {
-            if (err) next("Something went wrong!")
-            if (!res) next("Document not found or deleted")
-            console.log(result)
-        })
-
-    });
-
-    route.use(function(err, req, res) {
-        if (err) return res.send("err")
-    })
-
-    route.get('/:user/', function(req, res) {
-        db.article.fine({ author_id: req.params.user }, {}, function(err, result) {
-            console.log(result)
-        })
-    })
-    route.use((req, res, next) => {
-        res.send("Give us sth will ya ?")
-    })
     return route
 })();
